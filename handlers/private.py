943005a5a1200e53724715320a1d17c31a69b3e2
euna <vincreator123@gmail.com>
2021-04-16 06:33:19 +0700

    first clone

---
+++ handlers/private.py
@@ -0,0 +1,55 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+from helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+    await message.reply_sticker("CAACAgQAAx0CTv65QgABBfJlYF6VCrGMm6OJ23AxHmD6qUSWESsAAhoQAAKm8XEeD5nrjz5IJFYeBA")
+    await message.reply_text(
+        f"""**Hey, I'm {bn} 🎵
+
+I can play music in your group's voice call. Developed by [OVIN](https://t.me/oViNc).
+
+Add me to your group and play music freely!**
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "📩 Contact Me 📩", url="https://t.me/oViNc")
+                  ],[
+                    InlineKeyboardButton(
+                        "🌀 Powered 🌀", url="https://t.me/MultiAkunProduct"
+                  ],[      
+                    InlineKeyboardButton(
+                        "💬 Group", url="https://t.me/NollepAlliance"
+                    ),
+                    InlineKeyboardButton(
+                        "🔊 Channel", url="https://t.me/Namexian"
+                    )
+                ],[ 
+                    InlineKeyboardButton(
+                        "➕ Add To Your Group ➕", url="https://t.me/OurMusicplaybot?startgroup=true"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""**Group Music Player Online ✅**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔊 Channel", url="https://t.me/Namexian")
+                ]
+            ]
+        )
+   )
+
+
